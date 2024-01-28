f57fad740db029cc857a62ccaf262a7a97c89b57
DionMontolalu <dionmontolalu@gmail.com>
2024-01-28 19:30:10 +0800

    check token

--- lib/main.dart
+++ lib/main.dart
@@ -116,14 +116,6 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -131,22 +123,21 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Example Integration Flutter Firebase',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () async {
+          String? token = await FirebaseMessaging.instance.getToken();
+          print(token);
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
